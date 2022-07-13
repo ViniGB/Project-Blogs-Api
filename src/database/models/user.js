daaaae1352cc05a0ce3fd088aad3ee3ee4ab4dfb
Vinicius Gomes <viniciusemog@hotmail.com>
2022-07-13 17:43:35 -0300

    feat: (Req 02) Creates User Model

---
+++ src/database/models/user.js
@@ -0,0 +1,18 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  });
+
+  return User;
+}
+
+module.exports = User;
